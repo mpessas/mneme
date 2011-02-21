8ed0fdac732f2ecebba06bae7529cb31f23d77df
Apostolos Bessas <mpessas@gmail.com>
2011-02-21 11:45:01 +0200

    Use only xapian, implement add and search functionality

--- src/backend/datastore.py
+++ src/backend/datastore.py
@@ -2,49 +2,29 @@
 
 import os
 import os.path
-import sqlite3
+from indexstore import IndexStore
 
-class Data(object):
+class DataStore(object):
     """
     Class to save/retrieve data from datastore.
     
-    Uses xapian for indexing the data and an sqlite database
-    to save the indexed urls.
+    Uses xapian for indexing the data.
     """
 
-    def __init__(self):
-        self._data_dir = os.path.expanduser(u'~/.saveurl/')
-        self._sqlitedb_path = os.path.join(self._data_dir, u'urls.db')
+    def __init__(self, data_dir=None):
+        self._data_dir = data_dir
+        if self._data_dir is None:
+            self._data_dir = os.path.expanduser(u'~/.saveurl/')
         if not os.path.exists(self._data_dir):
             os.mkdir(self._data_dir)
-        if not os.path.exists(self._sqlitedb_path):
-            self._init_backend()
+        self.index = IndexStore(self._data_dir)
 
-    def _init_backend(self):
-        """Initialize the backend.
-        
-        The backend consists of a sqlite database serving as TOC and
-        a xapian database for indexing the urls.
-        """
-        self._init_sql()
-        self._init_xapian()
+    def add(self, url, text, categories=[]):
+        with self.index.connect():
+            self.index.add(url, text, categories)
 
-    def _init_sql(self):
-        """Initialize the sql table."""
-        self._conn = sqlite3.connect(self._sqlitedb_path)
-        c = self._conn.cursor()
-        query = "CREATE TABLE urls (id INTEGER PRIMARY KEY AUTOINCREMENT," + \
-                "url TEXT NOT NULL UNIQUE," + \
-                "xapian_id INTEGER NOT NULL UNIQUE)"
-        
-        c.execute(query)
-        self._conn.commit()
-        c.close()
-        self._conn.close()
-
-    def _init_xapian(self):
-        """Initialize the xapian backend."""
-        pass
-        
+    def search(self, text, category=None):
+        res = self.index.search(text, category)
+        return (self.index.get_url(r) for r in res)
         
     
