45f266195de8434846c905d0c9378ee7bfbf5ee5
Apostolos Bessas <mpessas@gmail.com>
2011-02-28 23:46:44 +0200

    Add setup file

---
+++ src/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='mneme',
+    version='0.1',
+    author='Apostolos Bessas',
+    author_email='mpessas@gmail.com',
+    description=('Index the content of urls locally for easier searching.'),
+    license='GPL',
+    packages=['store', 'document', ],
+    py_modules=['settings', 'mneme', ],
+    entry_points={
+        'console_scripts': [
+            'mneme=mneme:main',
+        ]
+    }
+)
