d564e5b15fe97a9d9629a1b0f0570ce719c1d042
Apostolos Bessas <mpessas@gmail.com>
2011-03-05 09:18:35 +0200

    Version info as function

--- mnemelib/__init__.py
+++ mnemelib/__init__.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.2'
+__version__ = '0.3'
+
+
+def get_version():
+    return __version__
--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 import os
 from setuptools import setup, find_packages
 
+from mnemelib import get_version
+
 scripts = ['mneme']
 
 setup(
     name='mneme',
-    version='0.2',
+    version=get_version(),
     scripts=scripts,
     author='Apostolos Bessas',
     author_email='mpessas@gmail.com',
