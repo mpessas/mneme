0df19d62da4189d52806c59106bc141716f7f4dd
Apostolos Bessas <mpessas@gmail.com>
2011-03-19 18:37:01 +0200

    Use urllib to read URls

--- mnemelib/document/document.py
+++ mnemelib/document/document.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import urllib
 import lxml.html
 
 
@@ -7,7 +8,8 @@ class Document(object):
     """A document to be indexed."""
 
     def __init__(self, url):
-        self.document = lxml.html.parse(url).getroot()
+        f = urllib.urlopen(url)
+        self.document = lxml.html.document_fromstring(f.read())
 
     def get_text(self):
         """Return the text from an html document."""
